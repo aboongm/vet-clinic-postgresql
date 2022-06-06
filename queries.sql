cc1e6172fa0bc242b8911ee302458e33491fef66
aboongm <aboongm@yahoo.com>
2022-06-06 14:56:45 +0530

    Queries for animals table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,11 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name LIKE '%mon%';
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-1-1' AND '2019-12-31';
+SELECT * from animals WHERE neutered IS TRUE AND escape_attempts < 3;
+SELECT date_of_birth from animals WHERE name IN ('Agumon','Pikachu');
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+SELECT * from animals WHERE neutered IS TRUE;
+SELECT * from animals WHERE name != 'Gabumon';
+SELECT * from animals WHERE weight_kg >= 10.5 AND weight_kg <= 17.3;
+
