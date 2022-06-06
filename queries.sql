455c06f796f6bf40a95d61541b3a90ac979b0bef
aboongm <aboongm@yahoo.com>
2022-06-06 20:32:48 +0530

    Update weights

--- queries.sql
+++ queries.sql
@@ -22,3 +22,10 @@ BEGIN;
 DELETE FROM animals;
 ROLLBACK;
 
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-1-1';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
