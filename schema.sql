671033315cc53598b3ebd1ec649d7af7f6643cfb
aboongm <aboongm@yahoo.com>
2022-06-07 17:26:54 +0530

    Modify animals id, add species_id and owners_id

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT,
+    id SERIAL,
     name VARCHAR(100),
     date_of_birth DATE,
     escape_attempts INT,
@@ -26,3 +26,14 @@ CREATE TABLE species (
     id INT PRIMARY KEY,
     name VARCHAR(100)
 );
+
+/* Remove species column */
+ALTER TABLE animals DROP COLUMN species;
+
+/* Add species_id in animals table referenced from species table */
+ALTER TABLE animals
+ADD COLUMN species_id INT REFERENCES species (id);
+
+/* Add owners_id in animals table referenced from owners table */
+ALTER TABLE animals
+ADD COLUMN owners_id INT REFERENCES owners (id);
