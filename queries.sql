e93cd72e78b2237a9569137b0a3d92b681255250
aboongm <aboongm@yahoo.com>
2022-06-07 19:22:55 +0530

    Queries using JOIN

--- queries.sql
+++ queries.sql
@@ -37,4 +37,77 @@ SELECT neutered, MAX(escape_attempts) from animals GROUP BY neutered;
 SELECT species, MAX(weight_kg), MIN(weight_kg) from animals GROUP BY species;
 SELECT species, AVG(escape_attempts) from animals WHERE date_of_birth BETWEEN '1990-1-1' AND '2000-12-31' GROUP BY species;
 
+/* queries using JOIN */
+/* animals belong to Melody Pond */
+SELECT 
+    name, full_name
+FROM
+    animals
+INNER JOIN owners
+    ON animals.owners_id = owners.id
+WHERE
+    owners.full_name = 'Melody Pond';
 
+/* all animals that are pokemon */
+SELECT 
+    animals.name
+FROM
+    animals
+INNER JOIN species
+    ON animals.species_id = species.id
+WHERE
+    species.name = 'Pokemon';
+
+/* all owners and their animals, remember to include those that don't own any animal */
+SELECT
+    name, full_name
+FROM
+    animals
+FULL JOIN owners
+    ON animals.owners_id = owners.id;
+
+/* How many animals are there per species */
+SELECT 
+    species.name, COUNT(*)
+FROM 
+    animals
+FULL JOIN species
+    ON animals.species_id = species.id
+    GROUP BY species.name;
+
+/* Digimon owned by Jennifer Orwell. */
+SELECT 
+    species.name, animals.name, owners.full_name
+FROM
+    animals
+FULL JOIN owners
+    ON animals.owners_id = owners.id
+FULL JOIN species
+    ON animals.species_id = species.id
+WHERE
+    owners.full_name = 'Jennifer Orwell'
+AND
+    species.name = 'Digimon';
+
+/* all animals owned by Dean Winchester that haven't tried to escape. */
+SELECT
+    animals.name, animals.escape_attempts
+FROM
+    animals
+FULL JOIN owners
+    ON animals.owners_id = owners.id
+WHERE
+    owners.full_name = 'Dean Winchester'
+AND
+    animals.escape_attempts = 0;
+
+/* owns the most animals */
+SELECT
+    owners.full_name, COUNT(animals.name)
+FROM
+    animals
+JOIN owners 
+    ON animals.owners_id = owners.id
+    GROUP BY owners.full_name
+    ORDER BY count DESC
+    LIMIT 1;
