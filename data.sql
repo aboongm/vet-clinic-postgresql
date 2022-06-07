c7c3512a1ccf62b1a6f539a8b545b29d8300be04
aboongm <aboongm@yahoo.com>
2022-06-07 18:07:24 +0530

    Update animals table with species_id and owners_id

--- data.sql
+++ data.sql
@@ -28,3 +28,13 @@ INSERT INTO species (id, name)
 VALUES 
 (DEFAULT, 'Pokemon'),
 (DEFAULT, 'Digimon');
+
+/* update animals */
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon%';
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon%';
+
+UPDATE animals SET owners_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owners_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owners_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owners_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owners_id = 5 WHERE name IN ('Angemon', 'Boarmon');
